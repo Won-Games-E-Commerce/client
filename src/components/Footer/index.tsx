4acd99119c0545229fc829deb83f39870cd83afd
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2021-08-04 22:37:06 -0300

    banner created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link'
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact
+        </Heading>
+        <a href="mailto:me@rodrigofernand.es">me@rodrigofernand.es</a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Follow us
+        </Heading>
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            rel="noopenner noreferrer"
+            target="_blank"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            rel="noopenner noreferrer"
+            target="_blank"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            rel="noopenner noreferrer"
+            target="_blank"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            rel="noopenner noreferrer"
+            target="_blank"
+          >
+            Facebook
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Links
+        </Heading>
+        <nav aria-labelledby="footer resources">
+          <Link href="/">Home</Link>
+          <Link href="/games">Store</Link>
+          <Link href="/search">Buscar</Link>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Location
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </S.Column>
+    </S.Content>
+
+    <S.Copyright>Won Games 2021 © All Rights Reserved</S.Copyright>
+  </S.Wrapper>
+)
+
+export default Footer
