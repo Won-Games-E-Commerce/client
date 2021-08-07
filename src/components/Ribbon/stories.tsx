d11a68ff419e7c729708a8c81734b7c0040df2fc
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2021-08-07 17:39:56 -0300

    created highlight component

--- src/components/Ribbon/stories.tsx
+++ src/components/Ribbon/stories.tsx
@@ -1,9 +1,17 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Ribbon from '.'
+import Ribbon, { RibbonProps } from '.'
 
 export default {
   title: 'Ribbon',
-  component: Ribbon
+  component: Ribbon,
+  args: {
+    children: 'Best Seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
 } as Meta
 
-export const Basic: Story = (args) => <Ribbon {...args} />
+export const Basic: Story<RibbonProps> = (args) => <Ribbon {...args} />
