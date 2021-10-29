e186e5388de94bfecd75b99b168ec33fe467c529
Rodrigo Fernandes <me@rodrigofernand.es>
2021-10-28 22:10:38 -0300

    adding cache and pagination to apollo

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -1,14 +1,21 @@
-import { gql } from '@apollo/client'
+import { gql, QueryHookOptions, useQuery } from '@apollo/client'
 import { GameFragment } from 'graphql/fragments/game'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export const QUERY_GAMES = gql`
-  query QueryGames($limit: Int!) {
-    games(limit: $limit) {
+  query QueryGames($limit: Int!, $start: Int) {
+    games(limit: $limit, start: $start) {
       ...GameFragment
     }
   }
   ${GameFragment}
 `
+
+export function useQueryGames(
+  options?: QueryHookOptions<QueryGames, QueryGamesVariables>
+) {
+  return useQuery<QueryGames, QueryGamesVariables>(QUERY_GAMES, options)
+}
 
 export const QUERY_GAME_BY_SLUG = gql`
   query QueryGameBySlug($slug: String!) {
