c7d6d874d6f71a7ffbe5dfa647000259e494092a
Rodrigo Fernandes <me@rodrigofernand.es>
2021-11-06 16:14:20 -0300

    authentication

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -23,16 +23,16 @@ export const parameters = {
 export const decorators = [
   (Story, context) => (
     <ThemeProvider theme={theme}>
-    <CartContext.Provider
-      value={{
-        ...CartContextDefaultValues,
-        ...(context?.args?.cartContextValue || {}),
-        ...context.args
-      }}
-    >
-      <GlobalStyles removeBg />
-      <Story />
-    </CartContext.Provider>
+      <CartContext.Provider
+        value={{
+          ...CartContextDefaultValues,
+          ...(context?.args?.cartContextValue || {}),
+          ...context.args
+        }}
+      >
+        <GlobalStyles removeBg />
+        <Story />
+      </CartContext.Provider>
     </ThemeProvider>
   ),
 ];
