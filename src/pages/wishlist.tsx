24d7c048bc106b6162e5ab1c19bb7a1e0ee6d763
Rodrigo Fernandes <me@rodrigofernand.es>
2021-11-13 11:59:24 -0300

    wishlist fix

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -2,17 +2,17 @@ import { initializeApollo } from 'utils/apollo'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import { gamesMapper, highlightMapper } from 'utils/mappers'
-import {
-  QueryWishlist,
-  QueryWishlistVariables
-} from 'graphql/generated/QueryWishlist'
-import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 
 import gamesMock from 'components/GameCardSlider/mock'
 import protectedRoutes from 'utils/protected-routes'
 import { GetServerSidePropsContext } from 'next'
+import {
+  QueryWishlist,
+  QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
@@ -22,12 +22,12 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
   const apolloClient = initializeApollo(null, session)
 
-  if (!session) return {}
+  if (!session) return { props: {} }
 
   await apolloClient.query<QueryWishlist, QueryWishlistVariables>({
     query: QUERY_WISHLIST,
     variables: {
-      identifier: session.user.email as string
+      identifier: session?.user?.email as string
     }
   })
 
--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,8 +1,8 @@
 import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
+import { Session } from 'next-auth'
 import { useMemo } from 'react'
 import apolloCache from './apolloCache'
-import { Session } from 'next-auth/client'
 
 let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
@@ -16,6 +16,7 @@ function createApolloClient(session?: Session | null) {
     const authorization = jwt ? `Bearer ${jwt}` : ''
     return { headers: { ...headers, authorization } }
   })
+
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: authLink.concat(httpLink),
@@ -27,13 +28,18 @@ export function initializeApollo(
   initialState = null,
   session?: Session | null
 ) {
+  // serve para verificar se já existe uma instância, para não criar outra
   const apolloClientGlobal = apolloClient ?? createApolloClient(session)
 
+  // se a página usar o apolloClient no lado client
+  // hidratamos o estado inicial aqui
   if (initialState) {
     apolloClientGlobal.cache.restore(initialState)
   }
 
+  // sempre inicializando no SSR com cache limpo
   if (typeof window === 'undefined') return apolloClientGlobal
+  // cria o apolloClient se estiver no client side
   apolloClient = apolloClient ?? apolloClientGlobal
 
   return apolloClient
